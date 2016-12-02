92688456c6b929395a23f387fa93bc2803c89b93
Gayathri Mohan <gayathrimohan@qburst.com>
2016-12-02 09:21:11 +0530

    installed dependencies and basic html structure

---
+++ src/app/app.ts
@@ -0,0 +1,6 @@
+import {Component} from 'angular2/core';
+@Component({
+	selector:'app',
+	template:require('./app.html')
+})
+export class AppComponent{ }
---
+++ src/bootstrap.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app/app';
+
+bootstrap(AppComponent)
+	.catch(err=>console.log(err));
